3b36ab6076f16faad65d36373ea9dc4c6f106840
jonatanMelzer <jonatan.melzer@ifolor.com>
2023-03-22 08:55:38 +0100

    adapt angular json budgets

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'free-editing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('free-editing');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
